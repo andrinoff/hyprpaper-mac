4be834137504e35a52a8a141470506e950d0afdd
drew <me@andrinoff.com>
2025-07-17 01:13:33 +0400

    fix: close UI on redirect
    When clicked on the buttons, it will automatically close the UI and open links in Finder, or Your Default Browser (GitHub)

--- preload.js
+++ preload.js
@@ -8,7 +8,8 @@ contextBridge.exposeInMainWorld("api", {
   getImageAsBase64: (fullPath) =>
     ipcRenderer.invoke("get-image-as-base64", fullPath),
   getThumbnail: (imageName) => ipcRenderer.invoke("get-thumbnail", imageName),
-  // **NEW**: Expose the new methods to the renderer process
   openWallpapersFolder: () => ipcRenderer.invoke("open-wallpapers-folder"),
   getAppVersion: () => ipcRenderer.invoke("get-app-version"),
+  // **FIXED**: Expose the method to open external links
+  openExternalLink: (url) => ipcRenderer.invoke("open-external-link", url),
 });
